e958e4d1e0aed3b9f9f7b04205ffbbb959bc779c
MAFTOUL OMAR <maftoul.omar@gmail.com>
2018-10-18 14:46:20 +0200

    Add files via upload

--- HiHostelFes.API/Migrations/20181016175907_mysqlmigration.Designer.cs
+++ HiHostelFes.API/Migrations/20181016175907_mysqlmigration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using HiHostelFes.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HiHostelFes.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20181016175907_mysqlmigration")]
+    partial class mysqlmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HiHostelFes.API/Migrations/20181016175907_mysqlmigration.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HiHostelFes.API.Migrations
+{
+    public partial class mysqlmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(nullable: true),
+                    PasswordHash = table.Column<byte[]>(nullable: true),
+                    PasswordSalt = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NameOfChambre = table.Column<string>(nullable: true),
+                    RoomStatus = table.Column<bool>(nullable: false),
+                    typeOfChambre = table.Column<string>(nullable: true),
+                    ChambreGender = table.Column<string>(nullable: true),
+                    numberOfBeds = table.Column<int>(nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    PhotoUrl = table.Column<string>(nullable: true),
+                    publicIdPhoto = table.Column<string>(nullable: true),
+                    startingFrom = table.Column<int>(nullable: false),
+                    initialPrice = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    CountryOfOrigin = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    DateOfArriving = table.Column<DateTime>(nullable: false),
+                    DateOfLeaving = table.Column<DateTime>(nullable: false),
+                    DateOfReservation = table.Column<DateTime>(nullable: false),
+                    DateOfCancellation = table.Column<DateTime>(nullable: false),
+                    numberOfClient = table.Column<int>(nullable: false),
+                    numberOfMale = table.Column<int>(nullable: false),
+                    numberOfFemale = table.Column<int>(nullable: false),
+                    numberOfChildren = table.Column<int>(nullable: false),
+                    numberOfNights = table.Column<int>(nullable: false),
+                    customerShowedHimSelf = table.Column<bool>(nullable: false),
+                    RoomId = table.Column<int>(nullable: false),
+                    Price = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Customers_Room_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Room",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    RoomId = table.Column<int>(nullable: false),
+                    CustomerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservation_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservation_Room_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Room",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_RoomId",
+                table: "Customers",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_CustomerId",
+                table: "Reservation",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_RoomId",
+                table: "Reservation",
+                column: "RoomId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "Reservation");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Room");
+        }
+    }
+}
