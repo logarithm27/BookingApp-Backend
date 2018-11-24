dad0e948e8cf46d4139c66a1244b8a04b39807f1
MAFTOUL OMAR <maftoul.omar@gmail.com>
2018-11-24 18:25:07 +0100

    Update CustomerForBlockingDto.cs

--- HiHostelFes.API/Dtos/CustomerForBlockingDto.cs
+++ HiHostelFes.API/Dtos/CustomerForBlockingDto.cs
@@ -2,10 +2,10 @@ namespace HiHostelFes.API.Dtos
 {
     public class CustomerForBlockingDto
     {
-         public int Id { get; set; }
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-         public int RoomId { get; set; }
-         public int numberOfChildren { get; set; }
+        public int RoomId { get; set; }
+
     }
 }
